a452eee9f5b57905643afe20d67861e7f2e5071b
vishnu-p007 <vishnu.pandey@hashicorp.com>
2024-09-27 18:17:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,3 +36,12 @@ resource "aws_instance" "app_server" {
     Name = "ExampleAppServerInstance"
   }
 }
+
+resource "aws_instance" "app_server2" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance2"
+  }
+}
